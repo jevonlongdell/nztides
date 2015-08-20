6e6f77661fae4723ca2b2d63a2750edfefaf4ab4
Jevon Longdell <jevon@physics.otago.ac.nz>
2015-08-20 21:50:08 +1200

    added in text files from Glen Rowe at LINZ and wrote a modified python script to turn them into binary

---
+++ tidelookup/binaryise_txt_tidetables.py
@@ -0,0 +1,106 @@
+#from datetime import datetime, timedelta
+#from pytz import timezone
+#import pytz
+import calendar
+import datetime
+import time
+import sys
+import os
+import struct
+import math
+import string
+lines = []
+
+#takes linz csv files and turns them into binary format for android app
+
+
+
+#ports = ['Auckland','Bluff','Dunedin','Gisborne','Lyttelton','Marsden Point','Napier','Nelson','Onehunga','Picton','Port Chalmers','Port Taranaki','Tauranga','Timaru','Wellington','Westport']
+
+
+ports = ['Anawhata', 'Auckland', 'Ben Gunn', 'Bluff', 'Castlepoint', 'Deep Cove', 'Dunedin', 'Fishing Rock', 'Flour Cask Bay', 'French Bay', 'Fresh Water Basin', 'Gisborne', 'Green Island', 'Havelock', 'Huruhi Harbour', 'Jackson Bay', 'Kaikoura', 'Kaingaroa', 'Kaiteriteri', 'Kaituna River', 'Kawhia', 'Korotiti Bay', 'Leigh', 'Lottin Point', 'Lyttelton', 'Mana', 'Man o\'War Bay', 'Mapua', 'Marsden Point', 'Matiatia Bay', 'Napier', 'Nelson', 'North Cape', 'Oamaru', 'Oban', 'Omokoroa', 'Onehunga', 'Opotiki Wharf', 'Opua', 'Owenga', 'Paratutae Island', 'Picton', 'Port Chalmers', 'Port Ohope Wharf', 'Port Taranaki', 'Pouto Point', 'Preservation Inlet', 'Raglan', 'Rocky Point', 'Scott Base', 'Spit Wharf', 'Sumner', 'Tarakohe', 'Tauranga', 'Timaru', 'Waiorua Bay', 'Waitangi (Chatham Is)', 'Wanganui', 'Wellington', 'Westport', 'Whakatane', 'Whangarei', 'Whangaroa', 'Whitianga']
+
+portfilenames = ['Anawhata', 'Auckland', 'Ben Gunn', 'Bluff', 'Castlepoint', 'Deep Cove', 'Dunedin', 'Fishing Rock', 'Flour Cask Bay', 'French Bay', 'Fresh Water Basin', 'Gisborne', 'Green Island', 'Havelock', 'Huruhi Harbour', 'Jackson Bay', 'Kaikoura', 'Kaingaroa', 'Kaiteriteri', 'Kaituna River', 'Kawhia', 'Korotiti Bay', 'Leigh', 'Lottin Point', 'Lyttelton', 'Mana', 'Mano War Bay', 'Mapua', 'Marsden Point', 'Matiatia Bay', 'Napier', 'Nelson', 'North Cape', 'Oamaru', 'Oban', 'Omokoroa', 'Onehunga', 'Opotiki Wharf', 'Opua', 'Owenga', 'Paratutae Island', 'Picton', 'Port Chalmers', 'Port Ophoe Wharf', 'Port Taranaki', 'Pouto Point', 'Preservation Inlet', 'Raglan', 'Rocky Point', 'Scott Base', 'Spit Wharf', 'Sumner', 'Tarakohe', 'Tauranga', 'Timaru', 'Waiorua Bay', 'Waitangi', 'Wanganui', 'Wellington', 'Westport', 'Whakatane', 'Whangarei', 'Whangaroa', 'Whitianga']
+
+
+#nztime = timezone('Pacific/Auckland')
+
+
+
+for kk in range(len(ports)):
+    port = ports[kk]
+
+    times = []
+    hts = []
+    print "Starting: " + port
+
+
+    for i in [2015,2016]:
+        try:
+            fp = open("txtfiles/%s_%d.txt"%(portfilenames[kk].replace(' ',''),i),'r')
+        except IOError:
+            print "couldn't open \"csvfiles/%s_%d.csv\", assuming we dont have any more data for %s"%(port,i,port)
+            break
+
+            
+        header =  fp.readline().strip()[3:].replace('  ',' ')
+        print header, port
+        assert(header==port) #these should be the same
+        fp.readline()
+        #print 
+        fp.readline()
+        
+        lines=[]
+        while True:
+            line = fp.readline()
+            if line == '':
+                break
+            lines.append(line.strip())
+
+
+        for line in lines:
+            f = [v for v in line.split(' ') if v!='']
+            day = int(f[0])
+            dow = f[1]
+            mon = int(f[2][:-2])
+            yr = int(f[2][-2:])
+            yr=yr+2000
+#            print day, dow, mon, yr
+            for k in range(3,len(f),2):
+                if f[k]=='9999':
+                    break
+                hr = int(f[k][:2])
+                mn = int(f[k][2:])
+                ht = float(f[k+1])
+                #tm = nztime.localize(datetime.datetime(yr,mon,day,hr,mn,0))
+                #times.append(int((tm - datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds()))
+                tm =  calendar.timegm((yr,mon,day,hr,mn,0,0,0,0))-12*60*60
+                times.append(tm)
+                hts.append(ht)
+                
+
+    of = open('%s.tdat'%(port.lower(),),'w')
+    print "writing %s.dat"%(port,)
+    #first line of tdat file is the port name
+    of.write('[%s]\n'%(string.capwords(port)))
+    #then an integer representing the date of the last tide
+    of.write(struct.pack('i',times[-1]))
+    print "the last time in this datafile will be " + time.asctime(time.localtime(times[-1]))
+    #then an integer representing the number of records
+    of.write(struct.pack('i',len(hts)))
+    print " the number of tide records is %d, which is about %g years worth"%(len(hts),len(hts)/(4.0*365))
+    #then for each record an integer valued time and a byte representing the height in decimeters
+    for k in range(len(hts)):
+        of.write(struct.pack('ib',times[k],int(round(hts[k]*10))))
+        #print time.asctime(time.localtime(times[k]))
+    of.close()
+    
+   # of = open('%s.txt'%(port,),'w')
+   # for k in range(len(hts)):
+   #     of.write('%d\n'%(times[k],))
+   # of.close()
+    print "-------------------------------"
+        
+    
+
+#test
