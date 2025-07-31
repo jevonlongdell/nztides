a923bf00903b8a96925cd30b88a4d9e10c042452
Rob Brooker <rob.brooker@woolworths.co.nz>
2025-07-31 17:34:18 +1200

    Add titlebar and basic menu

--- nztides_app_newui/app/src/main/java/com/palliser/nztides/MainActivity.kt
+++ nztides_app_newui/app/src/main/java/com/palliser/nztides/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -19,18 +20,25 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.palliser.nztides.ui.theme.NZTidesTheme
 import java.io.DataInputStream
 import java.io.IOException
@@ -51,6 +59,7 @@ class MainActivity : ComponentActivity() {
 
 
     //@Composable
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -58,18 +67,36 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NZTidesTheme {
-                Scaffold { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { 
+                                Text(
+                                    "NZ Tides",
+                                    fontSize = 18.sp
+                                ) 
+                            },
+                            actions = {
+                                MinimalDropdownMenu()
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Black,
+                                titleContentColor = Color.White,
+                                actionIconContentColor = Color.White
+                            ),
+                            windowInsets = WindowInsets(0.dp)
+                        )
+                    }
+                ) { innerPadding ->
                     val scrollState = rememberScrollState()
-                    //MinimalDropdownMenu()
                     Text(
                         calc_outstring(currentport),
                         modifier = Modifier
                             .padding(innerPadding)
                             .verticalScroll(scrollState),
-                        fontFamily = FontFamily.Monospace
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 13.sp
                     )
-
-
                 }
             }
         }
@@ -377,11 +404,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MinimalDropdownMenu() {
-        var expanded = false
-        //  var expanded  by remember { mutableStateOf(false) }
-        Box(
-            modifier = Modifier.padding(16.dp)
-        ) {
+        var expanded by remember { mutableStateOf(false) }
+        
+        Box {
             IconButton(onClick = { expanded = !expanded }) {
                 Icon(Icons.Default.MoreVert, contentDescription = "More options")
             }
@@ -390,12 +415,18 @@ class MainActivity : ComponentActivity() {
                 onDismissRequest = { expanded = false }
             ) {
                 DropdownMenuItem(
-                    text = { Text("Option 1") },
-                    onClick = { /* Do something... */ }
+                    text = { Text("Choose Port") },
+                    onClick = { 
+                        expanded = false
+                        // TODO: Open port selection
+                    }
                 )
                 DropdownMenuItem(
-                    text = { Text("Option 2") },
-                    onClick = { /* Do something... */ }
+                    text = { Text("About") },
+                    onClick = { 
+                        expanded = false
+                        // TODO: Show about dialog
+                    }
                 )
             }
         }
