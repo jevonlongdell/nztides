8e529661ae2129419761b5a6f576a8a9be906f05
Jevon Longdell <jevon.longdell@gmail.com>
2025-08-03 19:51:05 +0800

    update version number

--- nztides_app/app/build.gradle.kts
+++ nztides_app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.palliser.nztides"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 24
+        versionName = "24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
